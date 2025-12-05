58202c74f596eeb72cf6e61ca8d1e2691a9a79d2
almat101 <sandruozzo@gmail.com>
2025-12-05 23:37:15 +0100

    feat(server): implement graceful shutdown for server and database connections

--- backend/auth-service/server.js
+++ backend/auth-service/server.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
+import { getPool } from "../auth-service/config/database.js";
 
 const PORT = 3030;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Auth app listening on port ${PORT}`);
   console.log(`
           Route available:
@@ -10,3 +11,24 @@ app.listen(PORT, () => {
           '/auth/login' (POST)
           `);
 });
+
+// server.close wrapped into a promise to handle async/await
+const serverCloseAsPromise = () =>
+  new Promise((resolve, reject) => {
+    server.close((err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+
+async function shutdown(signal) {
+  console.log(`Received  ${signal}.Closing database and server...`);
+  const pool = getPool();
+  //close the server (ensure it does not accept new connections)
+  await serverCloseAsPromise();
+  //close the db connection pool
+  await pool.end();
+}
+
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGINT", () => shutdown("SIGINT"));
